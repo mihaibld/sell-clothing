60fb2be016e588a9603b06908d516745113918a4
Mihai Bledea <bledeam@gmail.com>
2024-02-04 19:58:30 +0200

    added product card component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { BrowserRouter } from 'react-router-dom';
+import { ProductsProvider } from './contexts/products.context';
+
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -12,7 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
